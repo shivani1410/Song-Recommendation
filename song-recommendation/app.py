69db47366f0f7a3500f81714f50adfd993202b57
shivani1410 <shivanimathur1410@gmail.com>
2025-08-10 19:54:35 +0530

    api part

---
+++ song-recommendation/app.py
@@ -0,0 +1,26 @@
+from flask import Flask,request,jsonify
+from flask_cors import CORS
+import pandas as pd
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+pkl_path = os.path.join(BASE_DIR, "song_recommender.pkl")
+
+df=pd.read_pickle(pkl_path)
+app=Flask(__name__)
+CORS(app)
+
+@app.route('/recommend-songs',methods=['POST'])
+def recommend_somgs():
+  data=request.get_json()
+  song_name=data['song']
+  
+  song=df[df['track_name'].str.lower()==song_name.lower()]
+  if song.empty:
+    return jsonify({"error":"Song not found"}),404
+  cluster_id=song['cluster'].values[0]
+  similiar_songs=df[df['cluster']==cluster_id].sample(5)
+  result=similiar_songs[['track_name','artist']].to_dict(orient='records')
+  return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
