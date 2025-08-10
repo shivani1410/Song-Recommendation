784c2f9cf434a260c68dffcc2e7e2abdfbe210be
shivani1410 <shivanimathur1410@gmail.com>
2025-08-10 20:34:32 +0530

    added logs

--- song-recommendation/app.py
+++ song-recommendation/app.py
@@ -2,16 +2,22 @@ from flask import Flask,request,jsonify
 from flask_cors import CORS
 import pandas as pd
 import os
+import logging
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 pkl_path = os.path.join(BASE_DIR, "song_recommender.pkl")
-
+logging.basicConfig(
+    level=logging.INFO,          # Minimum level to log
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
 df=pd.read_pickle(pkl_path)
 app=Flask(__name__)
 CORS(app)
 
 @app.route('/recommend-songs',methods=['POST'])
 def recommend_somgs():
+  logging.info("Application started") 
   data=request.get_json()
+  
   song_name=data['song']
   
   song=df[df['track_name'].str.lower()==song_name.lower()]
